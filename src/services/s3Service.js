f318597575ba22e7ac43f886c6e6aa783e31d0ab
Rian Moraes <rshmdev>
2025-06-14 17:52:39 -0300

    fix: s3

--- src/services/s3Service.js
+++ src/services/s3Service.js
@@ -1,21 +1,24 @@
-const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 
-const s3 = new AWS.S3({
+const s3 = new S3Client({
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 async function uploadFile(buffer, key, contentType) {
-  const params = {
+  const command = new PutObjectCommand({
     Bucket: process.env.S3_BUCKET_NAME,
     Key: key,
     Body: buffer,
     ContentType: contentType,
-    ACL: 'public-read',
-  };
-  const data = await s3.upload(params).promise();
-  return data.Location;
+  });
+
+  await s3.send(command);
+
+  return `https://${process.env.S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${key}`;
 }
 
 module.exports = { uploadFile };
