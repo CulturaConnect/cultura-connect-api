45b53eb4ff84bd5974251751b0f55509d82c98e6
Rian Moraes <98770810+rshmdev@users.noreply.github.com>
2025-07-07 22:19:20 -0300

    Restrict private project visibility

--- src/services/projectService.js
+++ src/services/projectService.js
@@ -1,4 +1,5 @@
 const { Project } = require('../models');
+const companyUserService = require('./companyUserService');
 
 async function createProject(data) {
   return Project.create(data);
@@ -6,6 +7,28 @@ async function createProject(data) {
 
 async function getProjects() {
   return Project.findAll();
+}
+
+async function userCanViewProject(project, user) {
+  if (project.is_public) return true;
+  if (!user) return false;
+  if (project.company_id) {
+    if (user.id === project.company_id) return true;
+    return companyUserService.userBelongsToCompany(project.company_id, user.id);
+  }
+  return (
+    user.id === project.responsavel_principal_id ||
+    user.id === project.responsavel_legal_id
+  );
+}
+
+async function getProjectsForUser(user) {
+  const projects = await getProjects();
+  const allowed = [];
+  for (const p of projects) {
+    if (await userCanViewProject(p, user)) allowed.push(p);
+  }
+  return allowed;
 }
 
 async function getProjectById(id) {
@@ -29,6 +52,8 @@ async function deleteProject(id) {
 module.exports = {
   createProject,
   getProjects,
+  getProjectsForUser,
+  userCanViewProject,
   getProjectById,
   updateProject,
   deleteProject,
